f2a7402db5261edb1fa5b75f7f65b8d326386ae3
ajiang1811 <89481585+ajiang1811@users.noreply.github.com>
2024-03-12 10:00:10 -0400

    Practice with mouse movement signals

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
-
-# For accessing command line arguments
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Finance Tracker")
+
+        self.label = QLabel("Click in this window")
+        self.setCentralWidget(self.label)
+
+    def mouseMoveEvent(self, e):
+        self.label.setText("Mouse moved")
+
+    def mousePressEvent(self, e):
+        self.label.setText("Mouse Pressed")
+
+    def mouseReleaseEvent(self, e):
+        self.label.setText("Mouse Released")
+
+    def mouseDoubleClickEvent(self, e):
+        self.label.setText("Mouse double clicked")
+
+
+
+
 
 # Establish QApplication instance for app, with ability to pass command line arguments.
 app = QApplication(sys.argv)
-app.setStyle("Fusion")
-label = QLabel("I'm Dumb!")
 
-label.show()
+# Create QtWidget main window
+window = MainWindow()
+window.show()
 
-app.exec_()
+# Start event loop
+app.exec()
