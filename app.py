3d27e32fcd61446094e4ec578211911703aafd9d
ajiang1811 <89481585+ajiang1811@users.noreply.github.com>
2024-03-14 14:57:15 -0400

    Establishing central GUI
    central widget centered and vertical layout skeleton made. Also moved the qapplication execution to guarded script.

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit
+from PyQt5.QtWidgets import (
+    QAction,
+    QApplication,
+    QFormLayout,
+    QGroupBox,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QMainWindow,
+    QLabel,
+    QLineEdit,
+    QTextEdit
+)
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.createUI()
 
+    def createUI(self):
+        # Creating main window
         self.setWindowTitle("Finance Tracker")
+        self.resize(800, 500)
+        self.setMinimumSize(500, 450)
 
-        self.label = QLabel("Click in this window")
-        self.setCentralWidget(self.label)
-
-    def mouseMoveEvent(self, e):
-        self.label.setText("Mouse moved")
-
-    def mousePressEvent(self, e):
-        self.label.setText("Mouse Pressed")
-
-    def mouseReleaseEvent(self, e):
-        self.label.setText("Mouse Released")
-
-    def mouseDoubleClickEvent(self, e):
-        self.label.setText("Mouse double clicked")
+        # Create Central widget and layout
+        self.centralWidget = QWidget()
+        self.verticalLayout = QVBoxLayout()
+        self.centralWidget.setLayout(self.verticalLayout)
+        # setting central widget
+        self.setCentralWidget(self.centralWidget)
 
 
 
+if (__name__ == "__main__"):
+    # Establish QApplication instance for app, with ability to pass command line arguments.
+    app = QApplication([])
 
+    # Create QtWidget main window
+    window = MainWindow()
+    window.show()
 
-# Establish QApplication instance for app, with ability to pass command line arguments.
-app = QApplication(sys.argv)
-
-# Create QtWidget main window
-window = MainWindow()
-window.show()
-
-# Start event loop
-app.exec()
+    # Start event loop
+    app.exec()
