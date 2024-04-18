572d05f7c74f39d5ab5bd5a26cfeaafd60966f0a
ajiang1811 <89481585+ajiang1811@users.noreply.github.com>
2024-04-18 15:08:26 -0400

    Added color palette

--- app.py
+++ app.py
@@ -12,7 +12,18 @@ from PyQt5.QtWidgets import (
     QTextEdit
 )
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QPalette, QColor
+
+# Custom widget for setting window color
+class Color(QWidget):
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,13 +37,13 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(500, 450)
 
         # Create Central widget and layout
-        self.centralWidget = QWidget()
+        self.centralWidget = Color('red')
         self.verticalLayout = QVBoxLayout()
         self.centralWidget.setLayout(self.verticalLayout)
         # setting central widget
         self.setCentralWidget(self.centralWidget)
 
-
+    
 
 if (__name__ == "__main__"):
     # Establish QApplication instance for app, with ability to pass command line arguments.
