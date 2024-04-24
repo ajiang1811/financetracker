e0a0abce3d09930aefd5ccf4e052ebdaeb076768
ajiang1811 <89481585+ajiang1811@users.noreply.github.com>
2024-04-23 20:45:45 -0400

    Added readme

--- mainwindow.py
+++ mainwindow.py
@@ -1,34 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication,QVBoxLayout, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox
-
+from PyQt5.QtWidgets import (
+    QDialog,
+    QMainWindow,
+    QApplication,
+    QVBoxLayout,
+    QWidget,
+    QPushButton,
+    QLabel,
+    QLineEdit,
+    QMessageBox,
+    QGridLayout
+)
 class LoginWindow(QDialog):
     def __init__(self, parent=None):
-        super(LoginWindow, self).__init__(parent)
+        super().__init__(parent)
         self.setWindowTitle('Login Form')
         self.setFixedSize(500, 300)
 
-        layout = QVBoxLayout(self)
+        self.layout = QVBoxLayout(self)
 
         welcome_message = QLabel('<font size="6"> Finance Tracker </font>')
-        layout.addWidget(welcome_message, 0)
+        self.layout.addWidget(welcome_message, 0)
 
         label_name = QLabel('<font size="4"> Username </font>')
         self.lineEdit_username = QLineEdit()
         self.lineEdit_username.setPlaceholderText('Please enter your username')
-        layout.addWidget(label_name, 1)
-        layout.addWidget(self.lineEdit_username, 1)
+        self.layout.addWidget(label_name, 1)
+        self.layout.addWidget(self.lineEdit_username, 1)
 
         label_password = QLabel('<font size="4"> Password </font>')
         self.lineEdit_password = QLineEdit()
         self.lineEdit_password.setPlaceholderText('Please enter your password')
-        layout.addWidget(label_password, 2)
-        layout.addWidget(self.lineEdit_password, 2)
+        self.layout.addWidget(label_password, 2)
+        self.layout.addWidget(self.lineEdit_password, 2)
 
         button_login = QPushButton('Login')
         button_login.clicked.connect(self.check_password)
-        layout.addWidget(button_login, 3)
+        self.layout.addWidget(button_login, 3)
 
-        self.setLayout(layout)
+        self.setLayout(self.layout)
 
     def check_password(self):
         msg = QMessageBox()
@@ -41,10 +51,17 @@ class LoginWindow(QDialog):
             msg.setText('Incorrect Password')
             msg.exec_()
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.widget = QWidget()
+        self.layout = QGridLayout()
+        self.setCentralWidget(widget)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    form = LoginWindow()
-    form.show()
+    window = MainWindow()
+    window.show()
 
     sys.exit(app.exec_())
